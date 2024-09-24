631cbea7632b1d1e5a0090d4f68689d27669d425
Vevaan <vevaan.s.verma@gmail.com>
2024-09-23 23:19:20 -0700

    Add loop print

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
-#include <iostream>
+// COMSC-210 | Lab 9 | Vevaan Verma
 using namespace std;
+#include <iostream>
 
+// main() is the entry point of the program
+// arguments: none
+// returns: int - the exit code of the program
 int main() {
-    // complete this code in order to make it output
-    // the numbers 1-100
 
-    for ()
-        cout << i << " ";
+	// complete this code in order to make it output
+	// the numbers 1-100
 
-    return 0;
+	// loop through the numbers [1, 100]
+	for (int i = 1; i <= 100; i++)
+		cout << i << " ";
+
+	return 0;
+
 }
